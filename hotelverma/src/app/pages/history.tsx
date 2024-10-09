f0452dd9e2fe33f6bf86cec4edc87600af7d14eb
Hy <60413204+Hy-KNG@users.noreply.github.com>
2024-10-09 00:39:18 -0700

    added navbar, page routing is broken atm

---
+++ hotelverma/src/app/pages/history.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function History() {
+  return (
+  <div style={{ display: "flex", justifyContent: "center", alignItems: "center", height: "100vh" }}>
+      <h1>Hello world!, this is a page about hotel history.</h1>
+  </div>
+  );
+}
