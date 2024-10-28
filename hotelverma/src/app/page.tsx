fb522c5eb4082729aa49d08aa9597e67de7159bd
JoshuaN0930 <joshua.guzman.7@my.csun.edu>
2024-10-28 14:23:41 -0700

    Update page.tsx

--- hotelverma/src/app/page.tsx
+++ hotelverma/src/app/page.tsx
@@ -5,16 +5,6 @@ import Image from "next/image";
 export default function Page() {
   return (
     <div className="">
-      <main className="">
-        <div className="container">
-          <h2 className="header">Quick Search</h2>
-          <button className="item1">Where to?</button>
-          <button className="item2">Dates</button>
-          <button className="item3">Travelers</button>
-          <button className="item5">Search</button>
-        </div>
-      </main>
-
       <footer className="row-start-3 flex gap-6 flex-wrap items-end justify-center">
         <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
