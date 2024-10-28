26bb9c9978f29822f67ac9ab525b27e88ac7449f
JoshuaN0930 <joshua.guzman.7@my.csun.edu>
2024-10-28 14:22:19 -0700

    Update layout.tsx

--- hotelverma/src/app/layout.tsx
+++ hotelverma/src/app/layout.tsx
@@ -2,6 +2,7 @@ import {Providers} from "./providers";
 import "./globals.css";
 import { Inter } from "next/font/google";
 import NavBar from "./components/navbar"
+import SearchBar from "./components/serchbar";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,6 +19,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
           <main>
             <NavBar>
             </NavBar>
+            <SearchBar></SearchBar>
               {children}
           </main>
         </Providers>
