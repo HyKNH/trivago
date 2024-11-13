5dbe5975b46b9de46bcaa0f7ab6836dafc020df0
JoshuaN0930 <joshua.guzman.7@my.csun.edu>
2024-11-13 12:58:09 -0800

    hotelverma/src/app/hotels/page.tsx

--- hotelverma/src/app/hotels/page.tsx
+++ hotelverma/src/app/hotels/page.tsx
@@ -1,11 +1,24 @@
 // app/pages/Hotels/page.tsx
-
+import {Autocomplete,AutocompleteItem} from "@nextui-org/react";
 
 export default function listings() {
   return ( 
-   <div className="flex">
-      <div>filter menu ---</div>
-      <div>contnet</div>
+   <div className="flex flex-wrap">
+      <div 
+        className="bg-white h-screen flex-initial w-1/4 text-black">
+          <h3 
+          className="text-xl font-500 mb-2 font-sans tracking-tighter ">
+            Search by hotel name
+            </h3>
+
+          <hr className=""/>
+          <button>2</button>
+      </div>
+      <div
+      className="ms-10"
+      >
+        content
+      </div>
    </div>
   );
 }
