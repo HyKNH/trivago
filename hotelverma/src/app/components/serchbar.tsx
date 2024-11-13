09ce563949cbcddc7dc6247570a9f66e0839a65e
HyKNH <lachynguyen70@gmail.com>
2024-11-13 14:39:30 -0800

    comment out

--- hotelverma/src/app/components/serchbar.tsx
+++ hotelverma/src/app/components/serchbar.tsx
@@ -38,7 +38,29 @@ export default function SearchBar() {
       <div className="flex flex-wrap gap-4 items-center justify-center w-full px-1">
         <h1 className="w-full text-xl font-bold flex-1">Quick Search</h1>
         <hr className="w-full border-t-2 border-gray-300 my-2 flex-initial"/>
-             
+      {/*
+        {colors.map((color) =>(
+             <Autocomplete 
+              className="text-orange-700 flex-initial ms-1 w-80" 
+              color = {"warning"}
+              key = {"warning"}
+              placeholder=""
+              label="Destination"
+              variant="faded"
+              defultItems={destination}
+              size="sm"
+            >
+            {destination.map((location) => (
+            <AutocompleteItem 
+            className="text-black mt-2"
+            key={location.value} 
+            value={location.value}>
+            {location.label}
+            </AutocompleteItem>
+          ))}
+          </Autocomplete>
+        ))}
+      */}
         <Popover>
           <PopoverTrigger>
             <Button className="w-72 flex-initial" variant="bordered" color="warning">{selectedRange}</Button>
@@ -52,7 +74,28 @@ export default function SearchBar() {
             />
             </div>
           </PopoverContent>
-        </Popover>   
+        </Popover>
+        {/*
+        <Dropdown>
+          <DropdownTrigger>
+            <Button className="flex-initial w-72"  variant="bordered">{selectedValue}</Button>
+          </DropdownTrigger>
+          <DropdownMenu
+           variant="flat"
+           disallowEmptySelection
+           selectionMode="single"
+           selectedKeys={selectedKeys}
+           onSelectionChange={setSelectedKeys}
+           color="warning"
+           className="text-black"
+          >
+            <DropdownItem key="Travelers: 1">Number of poeple: 1</DropdownItem>
+            <DropdownItem key="Travelers: 2">Number of People: 2</DropdownItem>
+            <DropdownItem key="Travelers: 3">Number of People: 3</DropdownItem>
+            <DropdownItem key="Travelers: 4">Number of People: 4</DropdownItem>
+          </DropdownMenu>
+        </Dropdown>   
+        */}
         <Button className="flex-initial w-32 border ml-[20px]" radius="full" color="primary">Search</Button>
       </div>
     </center>
