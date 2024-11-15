857a957fa1009ac4fc9df7e606bf064642c624f0
HyKNH <lachynguyen70@gmail.com>
2024-11-14 19:04:34 -0800

    added header

--- hotelverma/src/app/page.tsx
+++ hotelverma/src/app/page.tsx
@@ -4,6 +4,28 @@ import FeaturedPage from "./components/featured";
 
 export default function Page() {
   return (
-      <FeaturedPage/>
+    <>
+      <FeaturedPage />
+      
+      <footer style={footerStyle}>
+        <p style={textStyle}>
+          © 2024 Hotel Verma. All rights reserved. This website is a student project. Copyright-free images provided by Pexels.
+        </p>
+      </footer>
+    </>
   );
 }
+
+const footerStyle = {
+  position: "fixed",
+  bottom: 0,
+  width: "100%",
+  backgroundColor: "#f1f1f1",
+  textAlign: "center",
+  padding: "10px 0",
+};
+
+const textStyle = {
+  fontSize: "12px",
+  color: "#555", 
+};
