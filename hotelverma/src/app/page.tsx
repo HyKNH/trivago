f0adc6a3b19fb714d32c9a6b20acf28c1c3f32a2
HyKNH <lachynguyen70@gmail.com>
2024-11-14 19:08:03 -0800

    fixed

--- hotelverma/src/app/page.tsx
+++ hotelverma/src/app/page.tsx
@@ -1,4 +1,3 @@
-// app/page.tsx
 import * as React from "react";
 import FeaturedPage from "./components/featured";
 
@@ -7,25 +6,11 @@ export default function Page() {
     <>
       <FeaturedPage />
       
-      <footer style={footerStyle}>
-        <p style={textStyle}>
+      <footer className="footer-home">
+        <p className="footer-text-home">
           © 2024 Hotel Verma. All rights reserved. This website is a student project. Copyright-free images provided by Pexels.
         </p>
       </footer>
     </>
   );
 }
-
-const footerStyle = {
-  position: "fixed",
-  bottom: 0,
-  width: "100%",
-  backgroundColor: "#f1f1f1",
-  textAlign: "center",
-  padding: "10px 0",
-};
-
-const textStyle = {
-  fontSize: "12px",
-  color: "#555", 
-};
