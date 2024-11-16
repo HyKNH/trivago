d96e04e1f8e3a4bfdad54150f16711c69c1b2660
HyKNH <lachynguyen70@gmail.com>
2024-11-15 17:51:05 -0800

    small fix

--- hotelverma/src/app/forget/page.tsx
+++ hotelverma/src/app/forget/page.tsx
@@ -18,7 +18,7 @@ const ForgotPasswordPage = () => {
         setMessage("If this email is registered, you will receive a password reset link shortly.");
     };
     return (
-        <section className="min-h-screen flex flex-col items-center justify-center px-4 py-16 bg-gray-50">
+        <section className="min-h-screen flex flex-col items-center justify-center px-4 py-16 bg-gray-100">
             <div className="max-w-md w-full bg-white rounded-lg shadow-md p-8">
                 <h1 className="text-2xl font-bold mb-4 text-gray-800 text-center">Forgot Password</h1>
                 <p className="text-gray-600 mb-8 text-center">
