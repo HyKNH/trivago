9a3933c00a582245a574c612b3b7ab946f858328
JoshuaN0930 <joshua.guzman.7@my.csun.edu>
2024-11-26 21:46:59 -0800

    added payment method

--- hotelverma/src/app/reservation/page.tsx
+++ hotelverma/src/app/reservation/page.tsx
@@ -1,16 +1,52 @@
 "use client";
 import {Image} from "@nextui-org/image";
 import {Input} from "@nextui-org/input"
+import { useState } from "react";
 import { RiStarSFill } from "react-icons/ri";
 import Calendar from "../components/Calendar";
 
+
+
+
 export default function reservation() {
    const rating = 3;  // For now, set to a fixed value. Later, will fetch this from the database.
+
 
    // Create an array of size 'rating' using the Array constructor.
    // Then, use .fill() to assign a star icon to each element in the array.
    // This means, if 'rating' is 2, the array will contain two <RiStarSFill/> components.
    const stars = Array(rating).fill(<RiStarSFill/>)
+
+   const BIN = ['434256', '481592', '483312'];
+
+   const [cardNumber, setCardNumber] = useState("");//user card input
+   const [message,setMessage] = useState("");//mesage to display
+   const [showMessage, setShowMessage] = useState(false);// when to display message
+
+
+   //fucntion to handel carnd input 
+   const handleCardInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+      setCardNumber(e.target.value);
+      setMessage(""); // Reset message while typing
+    };
+  
+    //fucntion to handel submission of form 
+   const handleSubmit = (e : React.ChangeEvent<HTMLInputElement>) => {
+      e.preventDefault(); // Prevent form from reloading the page
+  
+        const firstSixDigits = cardNumber.slice(0, 6);
+        if (BIN.includes(firstSixDigits)) {
+          setMessage("Payment success");
+        } else {
+          setMessage("Payment failure! Please try again.");
+        }
+  
+      setShowMessage(true); // Show the message after clicking submit
+    };
+  
+ 
+ 
+  
 
     return ( 
       //parent div 
@@ -42,7 +78,8 @@ export default function reservation() {
       </div>
       <div className="pt-5 flex flex-wrap">
          <h1 className="mb-3 text-2xl w-full">Secure your Room</h1>
-         <form className="space-y-9 px-20  w-2/4">
+         {/*form for info on user*/}
+         <form className="space-y-9 px-20  w-2/4" onSubmit={handleSubmit}>
          <div>
             <h2 className="mb-5">Date for reservation</h2>
             <Calendar/>
@@ -87,9 +124,40 @@ export default function reservation() {
             startContent={<span>+1</span>}
          />
          <h2>Payment</h2>
-         <p className="h-44"> Payment Content</p>
+         <Input 
+            isRequired
+            label="Card Digits" 
+            type="text" 
+            placeholder="ex: 4342562412349087" 
+            required  
+            labelPlacement="outside"
+            size="md"
+            value={cardNumber}
+            onChange={handleCardInput}
+            />
+         <div className="flex gap-2">
+         <Input
+            isRequired
+            label="Expiraton Date"
+            labelPlacement="outside"
+            placeholder="MM/YY"
+            required
+            size="md"
+            className="w-1/2"
+         />
+         <Input
+            isRequired
+            label="CVV"
+            labelPlacement="outside"
+            placeholder="132"
+            required
+            size="md"
+            className="w-1/2"
+         />
+      </div>
+         <button  type="submit" className="border-1 border-stone-600 p-2 rounded-xl">Finish</button>
          </form>
-
+        
       <div className="border w-2/4 flex h-fit flex-wrap border-2 rounded-md shadow-xl justify-between p">
          <h1 className="text-xl text-semibold w-full">Price Details</h1>
          <h2 className="w-1/2 pt-5">1 night:</h2>
@@ -101,6 +169,7 @@ export default function reservation() {
          <h1 className="w-1/2">Total: </h1>
          <h1>$190.00</h1>
       </div>
+      { showMessage && <div className="text-green-500">{message}</div>}
       </div>
     </div>
     
