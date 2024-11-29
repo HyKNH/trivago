ffdd9bb8320205728611c22dcdd529b3c966fb49
rosalesangel <angel.rosales.023@my.csun.edu>
2024-11-28 19:52:43 -0800

    got the filters to print to console, will get them to work next

--- hotelverma/src/app/components/autoComplete.tsx
+++ hotelverma/src/app/components/autoComplete.tsx
@@ -1,6 +1,7 @@
 import {Autocomplete, AutocompleteItem} from "@nextui-org/react"
 
 export default function SeachSelect() {
+    const locations = ['random']
     return (
    // Autocomplete is used to get the component to render 
         <Autocomplete
@@ -8,6 +9,7 @@ export default function SeachSelect() {
             variant="bordered"
             color="warning"
             className="w-52 h-11"
+            onInputChange={(value) => console.log(value)}
         >
             {/*AutocompleteItem is used to add the content that will be rendered by Autocomplete which can be used 
             in two way one is to add items manually which means writting a AutocompleteItem for each item and assigning 
--- hotelverma/src/app/components/ratingFil.tsx
+++ hotelverma/src/app/components/ratingFil.tsx
@@ -1,34 +1,40 @@
 import { RiStarSFill } from "react-icons/ri";
 
 export default function Checkbox () {
+    const stars = {1: false, 2: false, 3: false, 4: false, 5: false}
+
+    function mychange(star: number) {
+        // @ts-expect-error-error
+        stars[star] = !stars[star]
+        console.log(stars)
+    }
     return (
         <div className="flex flex-wrap">
-                {/*first checkbox*/}
-                <div className="w-full flex">
-                <input type="checkbox" id="option1"  className="ml-5"/>
+            {/*first checkbox*/}
+            <div className="w-full flex">
+                <input type="checkbox" id="option1"  className="ml-5" onClick={() =>mychange(1)}/>
                 <label htmlFor="option1" className="flex pl-1">1 <RiStarSFill className="mt-1"/></label>
-                </div>
-                {/*second checkbox*/}
-                <div className="w-full flex">
-                <input type="checkbox" id=""  className="ml-5"/>
-                <label htmlFor="star" className="flex pl-1">2 <RiStarSFill className="mt-1"/></label>
-                </div>
-                {/*third checkbox*/}
-                <div className="w-full flex">
-                <input type="checkbox" id="option3"  className="ml-5"/>
-                <label htmlFor="option3" className="flex pl-1">3 <RiStarSFill className="mt-1"/></label>
-                </div>
-                {/*fouth checkbox*/}
-                <div className="w-full flex">
-                <input type="checkbox" id="option4"  className="ml-5"/>
-                <label htmlFor="option4" className="flex pl-1">4 <RiStarSFill className="mt-1"/></label>
-                </div>
-                {/*fith checkbox*/}
-                <div className="w-full flex">
-                <input type="checkbox" id="option5"  className="ml-5"/>
-                <label htmlFor="option5" className="flex pl-1">5 <RiStarSFill className="mt-1"/></label>
-                </div>
-                
             </div>
+            {/*second checkbox*/}
+            <div className="w-full flex">
+                <input type="checkbox" id="option2"  className="ml-5" onClick={() =>mychange(2)}/>
+                <label htmlFor="option2" className="flex pl-1">2 <RiStarSFill className="mt-1"/></label>
+            </div>
+            {/*third checkbox*/}
+            <div className="w-full flex">
+                <input type="checkbox" id="option3" className="ml-5" onClick={() =>mychange(3)}/>
+                <label htmlFor="option3" className="flex pl-1">3 <RiStarSFill className="mt-1"/></label>
+            </div>
+            {/*fouth checkbox*/}
+            <div className="w-full flex">
+                <input type="checkbox" id="option4" className="ml-5" onClick={() =>mychange(4)}/>
+                <label htmlFor="option4" className="flex pl-1">4 <RiStarSFill className="mt-1"/></label>
+            </div>
+            {/*fith checkbox*/}
+            <div className="w-full flex">
+                <input type="checkbox" id="option5" className="ml-5" onClick={() =>mychange(5)}/>
+                <label htmlFor="option5" className="flex pl-1" >5 <RiStarSFill className="mt-1"/></label>
+            </div>
+        </div>
     );
 }
