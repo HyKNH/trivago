500f72f82327194f2607a0951c686c447ad4b550
HyKNH <lachynguyen70@gmail.com>
2024-11-30 15:34:48 -0800

    added roles to accounts

--- hotelverma/src/app/signup/models/User.ts
+++ hotelverma/src/app/signup/models/User.ts
@@ -4,12 +4,14 @@ export interface IUser extends Document {
   name: string;
   email: string;
   password: string;
+  role: string;
 }
 
 const userSchema = new Schema<IUser>({
   name: { type: String, required: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
+  role: { type: String, enum: ['user', 'admin'], default: 'user' },
 });
 
 const User: Model<IUser> =
