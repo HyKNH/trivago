a106afde6306df51b49b6afe715abe90e44a9738
HyKNH <lachynguyen70@gmail.com>
2024-11-30 23:41:27 -0800

    refactor more

--- hotelverma/src/app/hotels/api/route.ts
+++ hotelverma/src/app/hotels/api/route.ts
@@ -4,28 +4,12 @@ import Hotel from '../models/Hotels';
 
 export async function GET(req: NextRequest) {
   try {
-
     await connectToDatabase();
 
     const hotels = await Hotel.find();
-
-    const formattedHotels = hotels.map((hotel) => ({
-      _id: hotel._id.toString(),
-      title: hotel.title,
-      location: hotel.location,
-      amenities: hotel.amenities || [],
-      image: hotel.image || "/default-image.jpg",
-      price: hotel.price,
-      rating: hotel.rating,
-    }));
-
-    return NextResponse.json({ hotels: formattedHotels }, { status: 200 });
+    return NextResponse.json({ hotels });
   } catch (error) {
     console.error('Error fetching hotels:', error);
-
-    return NextResponse.json(
-      { message: 'Error fetching hotels' },
-      { status: 500 }
-    );
+    return NextResponse.json({ message: 'Error fetching hotels' }, { status: 500 });
   }
 }
