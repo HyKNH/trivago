f5561ab35a4bb6556d03b1565967697380f7e2b1
HyKNH <lachynguyen70@gmail.com>
2024-11-30 23:36:52 -0800

    clean

--- hotelverma/src/app/hotels/api/route.ts
+++ hotelverma/src/app/hotels/api/route.ts
@@ -2,16 +2,13 @@ import { NextRequest, NextResponse } from 'next/server';
 import { connectToDatabase } from '../utils/db';
 import Hotel from '../models/Hotels';
 
-// Fetch all hotels
 export async function GET(req: NextRequest) {
   try {
-    // Connect to the database
+
     await connectToDatabase();
 
-    // Query all hotels from the database
     const hotels = await Hotel.find();
 
-    // Map hotels to a cleaner format if necessary
     const formattedHotels = hotels.map((hotel) => ({
       _id: hotel._id.toString(),
       title: hotel.title,
@@ -22,12 +19,10 @@ export async function GET(req: NextRequest) {
       rating: hotel.rating,
     }));
 
-    // Return the response
     return NextResponse.json({ hotels: formattedHotels }, { status: 200 });
   } catch (error) {
     console.error('Error fetching hotels:', error);
 
-    // Handle errors with a 500 response
     return NextResponse.json(
       { message: 'Error fetching hotels' },
       { status: 500 }
