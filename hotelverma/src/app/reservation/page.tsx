e5be9e7d65bab784aea1a76622acde4c8d937a94
JoshuaN0930 <joshua.guzman.7@my.csun.edu>
2024-12-01 20:45:29 -0800

    finished every thing ui wise for reservations

--- hotelverma/src/app/reservation/page.tsx
+++ hotelverma/src/app/reservation/page.tsx
@@ -1,12 +1,9 @@
 "use client";
 import { Image } from "@nextui-org/image";
 import { Input } from "@nextui-org/input";
-import {DateRangePicker} from "@nextui-org/react";
 import {useEffect, useState} from "react";
 import { RiStarSFill } from "react-icons/ri";
-import {today,getLocalTimeZone,  parseDate} from '@internationalized/date';
-import Calendar from "../components/Calendar";
-import axios from "axios"; // Make sure you import the Calendar component
+import axios from "axios"; 
 
 
 const BIN = ['434256', '481592', '483312'];
@@ -22,24 +19,77 @@ type Hotel = {
 };
 
 export default function Reservation() {
-  const rating = 3; // For now, set to a fixed value. Later, will fetch this from the database.
-
-  // Create an array of size 'rating' using the Array constructor.
-  // Then, use .fill() to assign a star icon to each element in the array.
-  // This means, if 'rating' is 2, the array will contain two <RiStarSFill/> components.
-  const stars = Array(rating).fill(<RiStarSFill />);
-
   const [cardNumber, setCardNumber] = useState(""); // User card input
   const [message, setMessage] = useState(""); // Message to display
   const [showMessage, setShowMessage] = useState(false); // When to display message
-  const [hotel, setHotel] = useState<Hotel | undefined>(undefined)
+  const [hotel, setHotel] = useState<Hotel | undefined>(undefined);
+  const [startDate, setStartDate] = useState("");
+  const [endDate, setEndDate] = useState("");
+  const [numNights, setNights] = useState(1);
+  const  [dateError, setDateError] = useState("");
+  const [showError, setShowError] = useState(false);
+  
 
+  const rating = hotel?.rating;
+  // Create an array of size 'rating' using the Array constructor.
+  // Then, use .fill() to assign a star icon to each element in the array.
+  // This means, if 'rating' is 2, the array will contain two <RiStarSFill/> components.
+  const stars = Array(rating).fill(<RiStarSFill className="text-yellow-400" />);
 
   // Function to handle card input
   const handleCardInput = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setCardNumber(e.target.value);
-    setMessage(""); // Reset message while typing
+    const inputValue = e.target.value;
+    setCardNumber(inputValue);
+    if (inputValue.length <16){
+      setMessage("Card Number must be 16-digits long")
+      setShowMessage(true);
+    }
+
+    else if (inputValue.length === 16){
+      setMessage("");//
+    }
   };
+
+  const handleDateInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+
+    if (name === "checkIn") {
+      setStartDate(value);
+    } else if (name === "checkOut") {
+      setEndDate(value);
+    }
+
+   
+  };
+
+
+  const calculateDays = () =>{
+    if (startDate && endDate){
+      const start = new Date(startDate);
+      const end = new Date(endDate);
+      start.setHours(0, 0, 0, 0);
+      end.setHours(0, 0, 0, 0);
+      const diffTime = end.getTime() - start.getTime();
+      const nights = Math.floor(diffTime / (1000 * 60 * 60 * 24));
+      setNights(nights);
+    }
+  }
+
+  useEffect(() => {
+    if (startDate && endDate){
+      const start = new Date (startDate);
+      const end = new Date(endDate);
+      if (end < start) {
+        setDateError("End date must come after start date");
+        setShowError(true);
+        setNights(1);
+        return;
+      } else if (end > start){
+        setDateError("")
+      }
+      calculateDays();
+    }
+  },[startDate, endDate]);
 
   // Function to get the right room
   useEffect(() => {
@@ -58,26 +108,52 @@ export default function Reservation() {
     fetchroom()
   }, []);
 
+  const calculateTotal = () => {
+    if (hotel?.price && numNights) {
+      const roomTotal = hotel.price * numNights;
+      const tax = 0.12
+      const taxTotal = roomTotal * tax;
+      const total = roomTotal + taxTotal;
+      return total;
+    }
+    return 0;
+  }
+
+  const calulateRoomT = () =>{
+      if (hotel?.price && numNights) {
+        return hotel.price * numNights;
+      }
+      return hotel?.price;
+  }
+  
+  
 
   // Function to handle form submission
   const submitForm = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault(); // Prevent form from reloading the page
 
-    const formData = new FormData(e.target as HTMLFormElement);
-    const payload = Object.fromEntries(formData);
-
-    console.log(payload);
-
-    /*const firstSixDigits = cardNumber.slice(0, 6);
+   // const hotelName = hotel?.title;
+    const firstSixDigits = cardNumber.slice(0, 6);
     if (BIN.includes(firstSixDigits)) {
-      setMessage("Payment success");
-    } else {
-      setMessage("Payment failure! Please try again.");
+      const formData = new FormData(e.target as HTMLFormElement);
+      const payload = Object.fromEntries(formData) as Record<string, string | undefined>;
+      payload.checkIn = payload.checkIn;  // Convert to ISO string for storage
+      payload.checkOut = payload.checkOut;
+      const conFirNum = Math.floor(Math.random() * 90000) + 10000;
+      payload.conFirNum = conFirNum.toString();
+      payload.title = hotel?.title;
+      console.log(payload);
+    }   
+    else {
+      setMessage("Sorry BIN number is invalid");
+      setShowMessage(true);
+      return;
     }
 
-    setShowMessage(true); // Show the message after clicking submit
-    */
   }
+
+
+
 
   return (
       <div className="px-6">
@@ -90,6 +166,8 @@ export default function Reservation() {
         </div>
         <hr className="w-full border-black mt-5" />
         <h1 className="mt-2 text-4xl">{hotel?.title}</h1>
+        <h2>{hotel?.location}</h2>
+        <h3>{hotel?.amenities}</h3>
         <div className="flex pt-3 pl-3">
           {stars.map((star, index) => (
               <div key={index}>{star}</div>
@@ -107,6 +185,8 @@ export default function Reservation() {
                 required
                 size="md"
                 name="checkIn"
+                onChange={handleDateInput}
+                value={startDate}
             />
             <Input
                 isRequired
@@ -116,7 +196,10 @@ export default function Reservation() {
                 required
                 size="md"
                 name="checkOut"
+                onChange={handleDateInput}
+                value={endDate}
             />
+            {showError && <p className="text-red-500">{dateError}</p>}
             <h2>Name for reservation</h2>
             <Input
                 isRequired
@@ -170,7 +253,11 @@ export default function Reservation() {
                 size="md"
                 value={cardNumber}
                 onChange={handleCardInput}
+                maxLength={16}
+                pattern="\d{16}"
+            i   nputMode="numeric"
             />
+            {showMessage && <div className="text-red-500">{message}</div>}
             <div className="flex gap-2">
               <Input
                   isRequired
@@ -196,18 +283,17 @@ export default function Reservation() {
             </button>
           </form>
 
-          <div className="border w-2/4 flex h-fit flex-wrap border-2 rounded-md shadow-xl justify-between p">
+          <div className="border w-2/4 flex h-fit flex-wrap border-2 rounded-md shadow-xl justify-between p-2">
             <h1 className="text-xl text-semibold w-full">Price Details</h1>
-            <h2 className="w-1/2 pt-5">1 night:</h2>
-            <h2 className="pr-2 pt-5">168.00</h2>
+            <h2 className="w-1/2 pt-5">Number of nights: {numNights}</h2>
+            <h2 className="pt-5">${calulateRoomT()}</h2>
             <h3 className="w-full pl-3">${hotel?.price} per night</h3>
             <h2 className="pt-5 w-1/2">Taxes and Fees:</h2>
-            <h2 className="pr-2 pt-5">22.00</h2>
+            <h2 className="pr-2 pt-5">%12</h2>
             <hr className="w-full border-black mt-5" />
-            <h1 className="w-1/2">Total: </h1>
-            <h1>$190.00</h1>
+            <h1 className="w-1/2">Total:</h1>
+            <h1>${calculateTotal()}</h1>
           </div>
-          {showMessage && <div className="text-green-500">{message}</div>}
         </div>
       </div>
   );
